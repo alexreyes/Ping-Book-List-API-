02c0daba496badaaa8d93ffb0699d8261679fd86
Alex Reyes <reyesruizalex@gmail.com>
2021-03-24 18:04:58 +0000

    added crypto prices pinging

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 
-url2021 = 'https://book-list-api-v2.alex243.repl.co/?year=2021'
+url2021books = 'https://book-list-api-v2.alex243.repl.co/?year=2021'
 
-x = requests.post(url2021)
+x = requests.post(url2021books)
 print(x.text)
+
+cryptoPrices = 'https://priceData.alex243.repl.co'
+
+y = requests.post(cryptoPrices)
+print(y.text)
